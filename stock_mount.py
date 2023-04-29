69434b3011a9afee6b750ba821b2df0ecf692154
Jonathan Haylett <jonathan@haylett.dev>
2023-04-29 12:33:24 +0100

    Clean up and add joints to PCB

--- stock_mount.py
+++ stock_mount.py
@@ -10,22 +10,13 @@ set_defaults(grid=(True, True, True), axes=True, axes0=True)
 
 # %%
 
-pcb_width = 107.6
-pcb_height = 60
-pcb_thickness = 1.6
 pcb_screw_x_spacing = 94.8
 pcb_screw_y_spacing = 53
-pcb_standoff_height = 6.8
-pcb_screw_hole_diameter = 2.8
-pcb_standoff_od = 6
-pcb_standoff_id = 3.5
+pcb_screw_hole_diam = 2.8
 
 lcd_height = 55.5
 lcd_width = 84.6
 lcd_thickness = 3.8
-
-lcd_screw_x_spacing = 96
-lcd_screw_y_spacing = 53
 
 # Measurements for the plate itself
 plate_height = 59
@@ -42,7 +33,7 @@ edge_cutout_height = 22
 
 
 class TouchscreenMount(Compound):
-    def __init__(self, pcb_screw_x_spacing, pcb_screw_y_spacing):
+    def __init__(self, pcb_screw_x_spacing, pcb_screw_y_spacing, pcb_screw_hole_diam):
         # Measurements for the points of attachment to the printer
         self.printer_mount_angle = 45
         self.printer_screw_x_spacing = 116
@@ -54,7 +45,7 @@ class TouchscreenMount(Compound):
         self.printer_screw_hole_id = 3.6
         self.printer_screw_hole_length = 10.5
         self.printer_screw_hole_counterbore_depth = 6.1
-        self.printer_screw_hole_counterbore_diameter = 6.8
+        self.printer_screw_hole_counterbore_diam = 6.8
         self.printer_screw_hole_protrusion_offset = math.tan(
             math.radians(self.printer_mount_angle)
         ) * 7.2 / 2
@@ -63,32 +54,37 @@ class TouchscreenMount(Compound):
         self.overall_width = self.printer_screw_x_spacing + padding / 2
         self.overall_height = self.printer_screw_y_spacing + padding
 
-        self.plate_part, pcb_screw_locations = self.plate(
+        self.plate_part = self.plate(
             pcb_screw_x_spacing,
             pcb_screw_y_spacing,
         )
         self.spacer_part: Part = self.spacer()
         self.pcb_part: Part = self.pcb()
 
-        spacers = []
-        for idx, loc in enumerate(pcb_screw_locations):
+        spacers: list[Part] = []
+        for idx, plate_spacer_joint in enumerate(self.plate_part.joints.values()):
             spacer_copy = copy.copy(self.spacer_part)
-            spacer_copy.label = f"PCB Spacer {idx}"
-            # Place rigid joint on plate
-            screw_hole_joint = RigidJoint(f"screw_hole{idx}", self.plate_part, loc)
-            # Place rigid joint on spacer
-            spacer_joint = RigidJoint("spacer", spacer_copy)
+            spacer_copy.label += f" {idx}"
             # Connect screw hole joint to spacer joint, which automatically positions the spacer
             # over the screw hole
-            screw_hole_joint.connect_to(spacer_joint)
+            plate_spacer_joint.connect_to(spacer_copy.joints["plate"])
             spacers.append(spacer_copy)
-    
+
+        for idx, pcb_spacer_joint in enumerate(self.pcb_part.joints.values()):
+            # Connect PCB joint to spacer joint, which automatically positions the PCB connected
+            # to the spacers
+            spacers[idx].joints["pcb"].connect_to(pcb_spacer_joint)
+        
         super().__init__(
             label="4Max Pro Touchscreen Mount",
-            children=[self.plate_part, self.pcb_part, *spacers],
+            children=[
+                self.plate_part,
+                self.pcb_part,
+                *spacers,
+            ],
         )
 
-    def plate(self, pcb_screw_x_spacing, pcb_screw_y_spacing) -> tuple[Part, list[Location]]:
+    def plate(self, pcb_screw_x_spacing, pcb_screw_y_spacing) -> Part:
         with BuildPart() as plate:
             # Base shape of mounting plate
             with BuildSketch() as base_sk:
@@ -145,48 +141,81 @@ class TouchscreenMount(Compound):
                     self.printer_screw_hole_length - self.printer_screw_hole_counterbore_depth
                 )
                 with BuildSketch(pln2) as counterbore_sk:
-                    Circle(self.printer_screw_hole_counterbore_diameter / 2)
+                    Circle(self.printer_screw_hole_counterbore_diam / 2)
                 extrude(amount=100, mode=Mode.SUBTRACT)
             
             # PCB screw holes
             with BuildSketch(bottom_face) as pcb_screw_holes_sk:
                 with GridLocations(pcb_screw_x_spacing, pcb_screw_y_spacing, 2, 2) as screw_holes:
-                    Circle(pcb_screw_hole_diameter / 2)
+                    Circle(pcb_screw_hole_diam / 2)
                     pcb_screw_locations = screw_holes.locations
             extrude(dir=(0, 0, 1), until=Until.LAST, mode=Mode.SUBTRACT)
 
+            for idx, loc in enumerate(pcb_screw_locations):
+                RigidJoint(f"spacer{idx}", plate.part, loc)
+
         plate.part.label = "Plate"
-        return plate.part, pcb_screw_locations
+        plate.part.color = "black"
+
+        return plate.part
 
     def spacer(self) -> Part:
+        outer_diam = 6
+        inner_diam = 3.5
+        height = 6.8
+
         # PCB spacers/standoffs
         with BuildPart() as spacer:
             with BuildSketch() as spacer_sk:
-                Circle(radius=pcb_standoff_od / 2)
-                Circle(radius=pcb_standoff_id / 2, mode=Mode.SUBTRACT)
-            extrude(amount=pcb_standoff_height)
+                Circle(radius=outer_diam / 2)
+                Circle(radius=inner_diam / 2, mode=Mode.SUBTRACT)
+            extrude(amount=height)
+
+        RigidJoint("plate", spacer.part)
+        RigidJoint("pcb", spacer.part, Location((0, 0, height)))
+
+        spacer.part.label = "PCB Spacer"
+        spacer.part.color = "grey"
 
         return spacer.part
     
     def pcb(self) -> Part:
+        pcb_width = 107.6
+        pcb_height = 60
+        pcb_thickness = 1.6
+
         with BuildPart() as pcb:
             with BuildSketch() as pcb_sk:
                 RectangleRounded(pcb_width, pcb_height, radius=2)
             extrude(amount=pcb_thickness)
 
+            bottom_face = pcb.part.faces().sort_by(Axis.Z).first
+
+            # PCB screw holes
+            with BuildSketch(bottom_face) as pcb_screw_holes_sk:
+                with GridLocations(pcb_screw_x_spacing, pcb_screw_y_spacing, 2, 2) as screw_holes:
+                    Circle(pcb_screw_hole_diam / 2)
+            extrude(dir=(0, 0, 1), until=Until.LAST, mode=Mode.SUBTRACT)
+
+            for idx, loc in enumerate(screw_holes.locations):
+                RigidJoint(f"spacer{idx}", pcb.part, loc)
+
         pcb.part.label = "PCB"
+        pcb.part.color = "green"
+
         return pcb.part
 
 
 assembly = TouchscreenMount(
     pcb_screw_x_spacing,
     pcb_screw_y_spacing,
+    pcb_screw_hole_diam,
 )
 
 show(
     assembly,
     render_joints=True,
-    transparent=True,
+    # transparent=True,
     reset_camera=False,
 )
 
